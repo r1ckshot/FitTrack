e17c9dcd026b1596224cbce92797909e3334f317
r1ckshot <kapusticnyk.com@gmail.com>
2025-04-16 19:50:37 +0200

    Login and Registration view

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -9,13 +9,22 @@ const router = express.Router();
 
 // Rejestracja użytkownika
 router.post('/auth/register', async (req, res) => {
-  const { username, email, password, role } = req.body;
+  const { username, email, password, role, accessCode } = req.body;
 
   try {
+    // Sprawdzanie roli i kodu dostępu
+    if (role === 'trainer' && accessCode !== process.env.TRAINER_ACCESS_CODE) {
+      return res.status(400).json({ error: 'Nieprawidłowy kod dostępu dla trenera.' });
+    }
+
+    if (role === 'admin' && accessCode !== process.env.ADMIN_ACCESS_CODE) {
+      return res.status(400).json({ error: 'Nieprawidłowy kod dostępu dla administratora.' });
+    }
+
     // Sprawdzenie, czy użytkownik istnieje w MongoDB
     const existingMongoUser = await MongoUser.findOne({ $or: [{ username }, { email }] });
     if (existingMongoUser) {
-      return res.status(400).json({ error: 'Użytkownik już istnieje w MongoDB.' });
+      return res.status(400).json({ error: 'Użytkownik już istnieje.' });
     }
 
     // Sprawdzenie, czy użytkownik istnieje w MySQL
@@ -23,7 +32,7 @@ router.post('/auth/register', async (req, res) => {
       where: { [Op.or]: [{ username }, { email }] } 
     });
     if (existingMySQLUser) {
-      return res.status(400).json({ error: 'Użytkownik już istnieje w MySQL.' });
+      return res.status(400).json({ error: 'Użytkownik już istnieje.' });
     }
 
     // Hashowanie hasła
@@ -68,12 +77,12 @@ router.post('/auth/login', async (req, res) => {
   
       // Weryfikacja hasła (MongoDB)
       if (mongoUser && !(await bcrypt.compare(password, mongoUser.password))) {
-        return res.status(401).json({ error: 'Nieprawidłowe hasło (MongoDB).' });
+        return res.status(401).json({ error: 'Nieprawidłowe hasło.' });
       }
   
       // Weryfikacja hasła (MySQL)
       if (mysqlUser && !(await bcrypt.compare(password, mysqlUser.password))) {
-        return res.status(401).json({ error: 'Nieprawidłowe hasło (MySQL).' });
+        return res.status(401).json({ error: 'Nieprawidłowe hasło.' });
       }
   
       // Generowanie tokena JWT
